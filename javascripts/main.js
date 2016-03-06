422f55529644e35210bb9b559451e44df440238d
Juan Herrera <david.juanherrera@gmail.com>
2016-03-06 15:40:24 -0500

    Map sections finished

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,7 +9,7 @@ $(function(){
 
 	$("#owl-example").owlCarousel({
 		items : 3,
-		autoPlay : 2500,
+		// autoPlay : 2500,
 		navigation : true,
 		navigationText : ["<",">"],
 		rewindNav : true,
@@ -19,3 +19,24 @@ $(function(){
 		itemsDesktop : [1199,3]
 	});
 });
+
+function initMap() {
+	var myCenter=new google.maps.LatLng(51.524293, -0.073912);
+	var mapDiv = document.getElementById('map');
+	var map = new google.maps.Map(mapDiv, {
+		center: myCenter,
+		zoom: 16
+	});
+	map.set("styles",[{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]);
+
+	var marker=new google.maps.Marker({
+		position:myCenter,
+		icon:'../images/marker.png'
+	});
+	marker.setMap(map);
+	var infowindow = new google.maps.InfoWindow({
+		content:"Geek Label<br> 4th Floor, 27-33 Benthnal Green Road<br>Shoreditch, London, E1 6LA",
+		anchorPoint : myCenter
+	});
+	infowindow.open(map,marker);
+}
