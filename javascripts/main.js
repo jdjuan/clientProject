a9b8af364dfc4b3aa25c35508b532a85d68d6c3b
Juan Herrera <david.juanherrera@gmail.com>
2016-03-06 13:18:52 -0500

    Clients section finished

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,21 @@
 $(function(){
 	$(".scrollButton").click(function(event) {
 		scrolling=true;
-		$('html,body').animate({
-			scrollTop: $("#"+$(this).attr("role")).offset().top},
+		$('html,body').animate({scrollTop: $("#"+$(this).attr("role")).offset().top},
 			1000, function(){
 				scrolling=false;
 			});
 	});
 
+	$("#owl-example").owlCarousel({
+		items : 3,
+		autoPlay : 2500,
+		navigation : true,
+		navigationText : ["<",">"],
+		rewindNav : true,
+		slideSpeed : 2000,
+		rewindSpeed : 2000,
+		paginationSpeed : 2000,
+		itemsDesktop : [1199,3]
+	});
 });
